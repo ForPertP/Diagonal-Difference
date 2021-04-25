c1ae8f49f3902b33f90f200577b230ebc5445087
ForPertP <50106601+ForPertP@users.noreply.github.com>
2021-04-25 17:23:56 +0900

    Create DiagonalDifference.cpp

---
+++ DiagonalDifference.cpp
@@ -0,0 +1,15 @@
+int diagonalDifference(vector<vector<int>> arr)
+{
+    size_t arr_size = arr.size() - 1;
+    
+    int left_diagonal_sum = 0;
+    int right_diagonal_sum = 0;
+    
+    for( size_t i = 0; i <= arr_size; ++i)
+    {
+        left_diagonal_sum += arr[i][i];
+        right_diagonal_sum += arr[i][arr_size-i];
+    }
+    
+    return abs(left_diagonal_sum - right_diagonal_sum);
+}
