64d748ab30f9ef58001d78244b71aefce2f2e5e8
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-23 07:51:02 +0900

    Update DiagonalDifference.cpp

--- DiagonalDifference.cpp
+++ DiagonalDifference.cpp
@@ -12,6 +12,7 @@ vector<string> split(const string &);
  * The function is expected to return an INTEGER.
  * The function accepts 2D_INTEGER_ARRAY arr as parameter.
  */
+
 int diagonalDifference(vector<vector<int>> arr)
 {
     size_t arr_size = arr.size() - 1;
